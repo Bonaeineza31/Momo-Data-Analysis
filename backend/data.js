b1da4ea90476882d3d050026aa87fa995e2d6a41
Bonaeineza31 <b.ineza@alustudent.com>
2025-06-12 13:00:56 +0200

    added data to postgress without duplicates

--- backend/data.js
+++ backend/data.js
@@ -3,8 +3,7 @@ const pool = require('./db');
 const parseXMLFile = require('./parse-xml');
 
 async function insertData() {
- const messages = await parseXMLFile(__dirname + '/data/modified-sms.xml');
-
+  const messages = await parseXMLFile(__dirname + '/data/modified-sms.xml');
 
   await pool.query(`
     CREATE TABLE IF NOT EXISTS transactions (
@@ -12,20 +11,23 @@ async function insertData() {
       type VARCHAR(50),
       amount INT,
       date TIMESTAMP,
-      body TEXT
+      body TEXT,
+      CONSTRAINT unique_transaction UNIQUE (type, amount, date, body)
     );
   `);
 
   for (const msg of messages) {
     if (msg.date && msg.amount > 0) {
       await pool.query(
-        `INSERT INTO transactions (type, amount, date, body) VALUES ($1, $2, $3, $4)`,
+        `INSERT INTO transactions (type, amount, date, body)
+         VALUES ($1, $2, $3, $4)
+         ON CONFLICT ON CONSTRAINT unique_transaction DO NOTHING;`,
         [msg.type, msg.amount, msg.date, msg.body]
       );
     }
   }
 
-  console.log(' Data inserted successfully!');
+  console.log('✅ Data inserted without duplication!');
   process.exit();
 }
 
